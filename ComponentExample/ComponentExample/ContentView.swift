6bd371c71f65249f2960bffa606ba2ad5556fd56
zhang.guanglu <zhang.guanglu@chinaredstar.com>
2019-10-23 15:47:01 +0800

    create project

---
+++ ComponentExample/ComponentExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComponentExample
+//
+//  Created by 张广路 on 2019/10/23.
+//  Copyright © 2019 symbool. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
